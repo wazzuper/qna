9a8b1064768ce4fe1a1bbfee6477575cadd2650a
artem <clifredd@gmail.com>
2017-02-22 13:26:26 +0500

    dependent option and few fixes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  it { should belong_to :question }
+
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-
-  it { should belong_to :question }
 end
