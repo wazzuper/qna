cf635ee4c6293dcb88fce8e8a033850e36be95dd
artem <clifredd@gmail.com>
2017-03-07 14:12:34 +0500

    index test, index view, qestion resources

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'slim-rails'
+gem 'rails-controller-testing'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
@@ -52,9 +54,7 @@ group :development do
   gem 'listen', '~> 3.0.5'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
-  gem 'spring-watcher-listen', '~> 2.0.0'
-
-  gem 'slim-rails'
+  gem 'spring-watcher-listen', '~> 2.0.0' 
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    #С помощью метода let мы выполняем код до тех пор, пока не вызовем его (15 строка)
+    #Один раз выполнившись, метод let сохраняет возвращенное значение
+    let(:questions) { create_list(:question, 2) }
 
+    before do
+      #Делаем запрос
+      get :index
+    end
+
+    it 'populates an array of all questions' do
+      #Сверяем @questions и созданные в бд два вопроса
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      #Ожидаем, что наш ответ от контроллера такой же, как шаблон index
+      expect(response).to render_template :index
+    end
+  end
 end
