6dca56eaadf2f787b6347b1fe3d510811092bad8
artem <clifredd@gmail.com>
2017-03-21 18:07:48 +0500

    ajax for answers (without create spec)

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -11,18 +11,18 @@ RSpec.describe AnswersController, type: :controller do
     context 'with valid attributes' do
       it 'saves the new question in the database' do
         #Мы ожидаем, что код выполненный в блоке изменит question.answers на 1 единицу
-        expect { post :create, params: { answer: attributes_for(:answer), question_id: question } }.to change(question.answers, :count).by(1)
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js } }.to change(question.answers, :count).by(1)
       end
 
-      it "render question" do
+      it "render create template" do
         #Отображаем вопрос с нашим ответом
-        post :create, params: { answer: attributes_for(:answer), question_id: question }
-        expect(response).to redirect_to question_path(assigns(:question))
+        post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
+        expect(response).to render_template :create
       end
 
       it 'this answer belongs to user' do
         #Сверяем answer.user и current_user
-        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
         expect(assigns(:answer).user_id).to eq @user.id
       end
     end
