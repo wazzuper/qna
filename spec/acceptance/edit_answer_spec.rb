b6ae33d60632c6d923c5ec77bd7348bdd9dfe08f
artem <clifredd@gmail.com>
2017-03-27 18:23:22 +0500

    fix create answer and edit answer tests

--- spec/acceptance/edit_answer_spec.rb
+++ spec/acceptance/edit_answer_spec.rb
@@ -9,9 +9,7 @@ feature 'Edit answer', %q{
   given(:user) { create(:user) }
   given(:user2) { create(:user) }
   given!(:question) { create(:question, user: user) }
-  given(:question2) { create(:question, user: user2) }
   given(:answer) { create(:answer, question: question, user: user) }
-  given(:answer2) { create(:answer, question: question2, user: user2) }
 
   scenario 'Non-authenticated trying to edit answer' do
     visit question_path(question)
@@ -37,21 +35,31 @@ feature 'Edit answer', %q{
         click_on 'Edit'
         fill_in 'Answer', with: 'Edited answer'
         click_on 'Save'
-        sleep(1)
-
+        
         expect(page).to_not have_content answer.body
         expect(page).to have_content 'Edited answer'
         expect(page).to_not have_selector 'textarea'
       end
     end
 
-    scenario 'can\'t edit other users answers' do
-      question2
-      answer2
-      visit question_path(question2)
+    scenario 'can\'t save empty answer box', js: true do
+      within '.answers' do
+        click_on 'Edit'
+        fill_in 'Answer', with: ''
+        click_on 'Save'
 
-      expect(page).to_not have_link 'Edit'
+        within '.answer-errors' do
+          expect(page).to have_content 'Body can\'t be blank'
+        end
+      end
     end
   end
 
+  scenario 'Authenticated user can\'t edit foreign answers' do
+    sign_in(user2)
+    answer
+    visit question_path(question)
+
+    expect(page).to_not have_link 'Edit'
+  end
 end
