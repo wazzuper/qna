49093248f6c0cef6f9a5e62004452c2b5326806e
artem <clifredd@gmail.com>
2017-03-29 19:53:56 +0500

    routes, acceptance test stories

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :answers
   resources :questions do
-    resources :answers, only: [:create, :update]
+    resources :answers, only: [:create, :update] do
+      patch 'set_best', on: :member
+    end
   end
 
   root to: 'questions#index'
