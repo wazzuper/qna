209a13cb2c319082baa4909e916ba840ebd0a6c7
artem <clifredd@gmail.com>
2017-04-03 23:50:36 +0500

    finished with add file to answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
   it { should belong_to :question }
+  it { should have_many(:attachments).dependent(:destroy) }
 
   it { should validate_presence_of :body }
+
+  it { should accept_nested_attributes_for :attachments }
 
   describe '#set_best' do
     let!(:user) { create(:user) }
