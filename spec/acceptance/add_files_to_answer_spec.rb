5b5e2242fb3531aefd526c58376d2412ac29499b
artem <clifredd@gmail.com>
2017-04-04 19:06:38 +0500

    user can add two or more files to question and answer

--- spec/acceptance/add_files_to_answer_spec.rb
+++ spec/acceptance/add_files_to_answer_spec.rb
@@ -16,11 +16,33 @@ feature 'Add files to answer', %q{
 
   scenario 'User add file when he give an answer', js: true do
     fill_in 'Answer', with: 'Interesting answer'
+    click_on 'Add file'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Give an answer'
 
     within '.answers' do
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end
+  end
+
+  scenario 'User add two or more files when he give an answer', js: true do
+    fill_in 'Answer', with: 'Interesting answer'
+
+    click_on 'Add file'
+    within page.all('.nested-fields')[0] do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on 'Add file'
+    within page.all('.nested-fields')[1] do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+
+    click_on 'Give an answer'
+
+    within '.answers' do
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
     end
   end
 end
--- spec/acceptance/add_files_to_question_spec.rb
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -13,12 +13,33 @@ feature 'Add files to question', %q{
     visit new_question_path
   end
 
-  scenario 'User add file when he ask question' do
+  scenario 'User add one file when he ask question', js: true do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'Test body'
+    click_on 'Add file'
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
     click_on 'Create'
 
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  end
+
+  scenario 'User add two or more files when he ask question', js: true do
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Test body'
+
+    click_on 'Add file'
+    within page.all('.nested-fields')[0] do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+
+    click_on 'Add file'
+    within page.all('.nested-fields')[1] do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+
+    click_on 'Create'
+
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
   end
 end
